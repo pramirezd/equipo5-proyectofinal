721d87e06a534a22063ab2e3598fba04ac6bfc07
Ignacio Tapia <itapia@falabella.cl>
2024-09-08 16:53:41 -0300

    Refactor cart controller, model and routes

--- backend/models/cartModel.js
+++ backend/models/cartModel.js
@@ -5,7 +5,7 @@ import pool from "../config/db.js";
 const getUserCart = async (id) => {
   try {
     const query = `
-            SELECT cp.id, p.name, p.price, cp.quantity
+            SELECT cp.product_id, p.name, p.price, cp.quantity
             FROM carts c
             JOIN cart_product cp ON c.id = cp.cart_id
             JOIN products p ON cp.product_id = p.id
@@ -54,7 +54,46 @@ const addProduct = async (user_id, product_id, quantity) => {
     throw new Error("Hubo un error con la operación ADDPRODUCT");
   }
 };
+
+const removeProduct = async (user_id, product_id) => {
+  try {
+    // Verificar si el carrito del usuario existe
+    const cartQuery = `
+      SELECT id FROM carts WHERE user_id = $1;
+    `;
+    const cartResult = await pool.query(cartQuery, [user_id]);
+
+    // Si el carrito no existe, lanzar un error
+    if (cartResult.rows.length === 0) {
+      throw new Error("El carrito no existe para este usuario.");
+    }
+
+    const cart_id = cartResult.rows[0].id;
+
+    // Eliminar el producto del carrito
+    const deleteQuery = `
+      DELETE FROM cart_product 
+      WHERE cart_id = $1 AND product_id = $2
+      RETURNING *;
+    `;
+
+    const deleteResult = await pool.query(deleteQuery, [cart_id, product_id]);
+    console.log(deleteResult.rows);
+
+    // Si el producto no está en el carrito, devolver un mensaje
+    if (deleteResult.rows.length === 0) {
+      return { message: "El producto no se encontró en el carrito." };
+    }
+
+    return { message: "Producto eliminado del carrito con éxito." };
+  } catch (error) {
+    console.error("Error al eliminar producto del carrito:", error);
+    throw new Error("Hubo un error al eliminar el producto del carrito.");
+  }
+};
+
 export const cartModel = {
   getUserCart,
   addProduct,
+  removeProduct,
 };
--- backend/routes/carts.routes.js
+++ backend/routes/carts.routes.js
@@ -1,11 +1,20 @@
 import { cartController } from "../controllers/cartController.js";
-import express from 'express'
+import express from "express";
 import { AuthMiddleware } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 //Ruta para obtener los carritos por usuario
-router.get("/", AuthMiddleware.authToken,cartController.getCart)
+router.get("/user/:user_id", AuthMiddleware.authToken, cartController.getCart);
 //Ruta para agregar un producto al carrito
-router.post("/add", AuthMiddleware.authToken, cartController.addToCart)
+router.post(
+  "/user/:user_id",
+  AuthMiddleware.authToken,
+  cartController.addToCart
+);
+router.delete(
+  "/user/:user_id/product/:product_id",
+  AuthMiddleware.authToken,
+  cartController.removeProduct
+);
 
 export default router;
