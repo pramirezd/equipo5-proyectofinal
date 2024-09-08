7d920f70f7a890af8e17198ce4dad523b11b1873
Ignacio Tapia <itapia@falabella.cl>
2024-09-08 16:54:39 -0300

    Refactor order controller, model and routes

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,21 +1,30 @@
 import { orderController } from "../controllers/orderController.js";
-import express from 'express'
+import express from "express";
+import { AuthMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-//Ruta para obtener las ordenes de un usuario
-router.get('/user/:user_id', orderController.getUserOrders);
-
 //Ruta para obtener todas las ordenes
-router.get('/allOrders', orderController.getAllOrders);
+router.get("/allOrders", AuthMiddleware.isAdmin, orderController.getAllOrders);
+
+//Ruta para obtener las ordenes de un usuario
+router.get(
+  "/user/:user_id",
+  AuthMiddleware.authToken,
+  orderController.getUserOrders
+);
 
 //Ruta para crear una orden
-router.post('/user/:user_id', orderController.addOrder);
+router.post(
+  "/user/:user_id",
+  AuthMiddleware.authToken,
+  orderController.addOrder
+);
 
 //Ruta para actualizar una orden
-router.put('/:id/user/:user_id', orderController.updateOrder);
+router.put("/:order_id", AuthMiddleware.isAdmin, orderController.updateOrder);
 
 //Ruta para eliminar una orden
-router.delete('/:id/user/:user_id', orderController.deleteOrder);
+router.delete("/:order_id", orderController.deleteOrder);
 
 export default router;
