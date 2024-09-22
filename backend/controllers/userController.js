c26e640afa2c7b8f3cdb7ae3fb59ade1087bdb4f
Ignacio Tapia <itapia@falabella.cl>
2024-09-22 19:50:56 -0300

    rollback

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,8 +55,7 @@ const register = async (req, res) => {
     const cookieOptions = {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: process.env.NODE_ENV === "PROD" ? "none" : "lax", // Para permitir cookies entre sitios en producción
-      secure: process.env.NODE_ENV === "PROD", // Solo cookies seguras en producción
+      sameSite: "lax",
     };
     res.cookie("token_access", token, cookieOptions);
     return res.status(201).json({
@@ -98,8 +97,7 @@ const login = async (req, res) => {
     const cookieOptions = {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: process.env.NODE_ENV === "PROD" ? "none" : "lax", // Para permitir cookies entre sitios en producción
-      secure: process.env.NODE_ENV === "PROD", // Solo cookies seguras en producción
+      sameSite: "lax",
     };
     res.cookie("token_access", token, cookieOptions);
     console.log(token);
